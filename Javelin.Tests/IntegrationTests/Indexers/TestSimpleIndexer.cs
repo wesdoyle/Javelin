a708d85969668d8fac234deb195c85e10093907d
wesdoyle <wesdoyle08@gmail.com>
2020-07-19 19:32:01 -0500

    Update test fixtures

--- Javelin.Tests/IntegrationTests/Indexers/TestSimpleIndexer.cs
+++ Javelin.Tests/IntegrationTests/Indexers/TestSimpleIndexer.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using Javelin.Indexers;
 using Javelin.Serializers;
 using Javelin.Tokenizers;
@@ -5,12 +6,17 @@ using Xunit;
 
 namespace Javelin.Tests.IntegrationTests.Indexers {
     public class TestSimpleIndexer {
+        
+        private readonly string _testDirectory = 
+            Directory.GetParent(Directory.GetCurrentDirectory())
+                .Parent?.FullName;
+        
         [Fact]
         public void Test_SimpleIndexer_Builds_SimpleInvertedIndex() {
             var tokenizer = new SimpleTokenizer();
             var serializer = new BinarySerializer<SimpleInvertedIndex>();
             var sut = new SimpleIndexer(tokenizer, serializer);
-            sut.BuildIndexForArchive("./TestFixtures/Data.zip", "./TestFixtures/TestIndex");
+            sut.BuildIndexForArchive("./TestFixtures/Data.zip", Path.Join(_testDirectory, "TestIndex"));
         }
     }
 }
